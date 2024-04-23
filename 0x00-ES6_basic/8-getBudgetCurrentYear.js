4e50def10b41fefc2dc4e70433f1c66d19871fd6
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-04-23 02:47:01 +0200

    Update 8-getBudgetCurrentYear.js

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,12 @@
+// File: 8-getBudgetCurrentYear.js
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = new Date().getFullYear();
+
+  const budget = {
+    [`income-${currentYear}`]: income,
+    [`gdp-${currentYear}`]: gdp,
+    [`capita-${currentYear}`]: capita,
+  };
+
+  return budget;
+}
