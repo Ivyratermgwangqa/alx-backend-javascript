8044379dc3b733e4d5c6127897575414b8c1dcc9
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-01 01:26:43 +0200

    Update babel.config.js

--- 0x03-ES6_data_manipulation/babel.config.js
+++ 0x03-ES6_data_manipulation/babel.config.js
@@ -1,25 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:jest/recommended",
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current", // Targeting the current version of Node.js
+        },
+        useBuiltIns: "usage", // Include polyfills only when needed
+        core-js: 3, // Specifies the version of core-js for polyfills
+        modules: false, // Retain ES6 module syntax
+        loose: true, // Use "loose" transformations
+      },
+    ],
   ],
-  parserOptions: {
-    ecmaVersion: 12, // Equivalent to ECMAScript 2021
-    sourceType: "module",
-  },
-  rules: {
-    "indent": ["error", 2], // Enforce 2-space indentation
-    "quotes": ["error", "single"], // Prefer single quotes
-    "semi": ["error", "always"], // Require semi-colons at the end of statements
-    "no-console": "warn", // Warn if console.log is used
-    "no-unused-vars": ["warn"], // Warn for unused variables
-    "space-before-function-paren": ["error", "always"], // Enforce space before function parenthesis
-  },
-  plugins: ["jest"], // Adding Jest plugin to support Jest linting rules
 };
