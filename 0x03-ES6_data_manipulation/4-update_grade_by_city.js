4658faddbb3f6bde3069061230da7522724f564d
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-01 04:56:04 +0200

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,7 @@
-export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const gradeEntry = newGrades.find((grade) => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: gradeEntry ? gradeEntry.grade : 'N/A',
-      };
-    });
+const getListStudents = require('./0-get_list_students.js');
+
+function getStudentIdsSum(list) {
+  return list.reduce((sum, student) => sum + student.id, 0);
 }
+
+module.exports = getStudentIdsSum;
