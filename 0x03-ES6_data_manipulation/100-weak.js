661f59c1ba3f922cb7ca5ab9f059b299bb1472d5
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-01 02:00:43 +0200

    Update 100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,12 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  let callCount = weakMap.get(endpoint) || 0;
+
+  callCount += 1;
+  weakMap.set(endpoint, callCount);
+
+  if (callCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
