2e024b4987d0bda1070b99fba794d1204d88ad69
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-01 04:55:08 +0200

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,10 @@
-export default function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
+const getListStudents = require('./0-get_list_students.js');
+
+function getListStudentIds(list) {
+  if (!Array.isArray(list)) {
     return [];
   }
-  return students.map((student) => student.id);
+  return list.map((student) => student.id);
 }
+
+module.exports = getListStudentIds;
