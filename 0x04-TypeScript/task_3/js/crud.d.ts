edca639bfde4de5d3a6f9b1c7283746ab8ee1639
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-02 04:57:43 +0200

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,18 +1,3 @@
-// CRUD operations
-export function insertRow(row) {
-  console.log('Insert row', row);
-  return Math.floor(Math.random() * 1000);
-}
-
-export function deleteRow(rowId) {
-  console.log('Delete row id', rowId);
-}
-
-export function updateRow(rowId, row) {
-  console.log(`Update row ${rowId}`, row);
-  return rowId;
-}
-
 import { RowID, RowElement } from './interface';
 
 // Ambient type declarations for CRUD operations
