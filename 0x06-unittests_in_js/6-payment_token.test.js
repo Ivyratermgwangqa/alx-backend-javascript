5b8c49d1e671b3dcf96d4d1a082b320822425c7c
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-06-21 05:32:22 +0200

    Update 6-payment_token.test.js

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const expect = chai.expect;
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+    it('should return a resolved promise with data when success is true', function(done) {
+        getPaymentTokenFromAPI(true).then(response => {
+            expect(response).to.deep.equal({ data: 'Successful response from the API' });
+            done();
+        }).catch(done);
+    });
+});
